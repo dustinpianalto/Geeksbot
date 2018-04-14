69fab0e7a37fddba926a8079fb16a823cb8a20d3
Dusty.P <dustin@djpianalto.com>
2018-04-14 03:22:58 -0800

    Updated Email Checker

--- exts/git.py
+++ exts/git.py
@@ -26,8 +26,8 @@ class Git():
                             color=embed_color)
         em.set_thumbnail(url=f'{ctx.guild.me.avatar_url}')
         result = await asyncio.wait_for(self.bot.loop.create_task(run_command('git fetch --all')),120) + '\n'
-        result += await asyncio.wait_for(self.bot.loop.create_task(run_command('git reset --hard origin/master')),120) + '\n'
-        result += await asyncio.wait_for(self.bot.loop.create_task(run_command('git show --stat | sed -r "s/^(.*? |)[^@]+@[^ ]+/\1email/g"')),10)
+        result += await asyncio.wait_for(self.bot.loop.create_task(run_command('git reset --hard origin/master')),120) + '\n\n'
+        result += await asyncio.wait_for(self.bot.loop.create_task(run_command('git show --stat | sed "s/.*@.*[.].*/ /g"')),10)
         results = paginate(result, maxlen=1014)
         for page in results[:5]:
             em.add_field(name='￲', value=f'{page}')
