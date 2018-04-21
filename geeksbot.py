13d4dc564ca33514bb1282dde8bc1016a5a5f7ef
Dusty.P <dustin@djpianalto.com>
2018-04-20 20:58:24 -0800

    Switching to asyncpg

--- geeksbot.py
+++ geeksbot.py
@@ -65,10 +65,10 @@ class Geeksbot(commands.Bot):
         self.gcs_service = build('customsearch', 'v1', developerKey=self.bot_secrets['google_search_key'])
 
     async def connect_db(self):
-        return await asyncpg.connect(f"host={self.bot_secrets['db_con']['host']}\
-                                     database={self.bot_secrets['db_con']['db_name']}\
-                                     user={self.bot_secrets['db_con']['user']}\
-                                     password={self.bot_secrets['db_con']['password']}")
+        return await asyncpg.connect(host={self.bot_secrets['db_con']['host']},
+                                     database={self.bot_secrets['db_con']['db_name']},
+                                     user={self.bot_secrets['db_con']['user']},
+                                     password={self.bot_secrets['db_con']['password']})
 
     @staticmethod
     async def get_custom_prefix(bot_inst, message):
