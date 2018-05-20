960fbfbcd2f1d5324a98a9247934ef4f0cf8b06b
Dusty.P <dustin@djpianalto.com>
2018-05-20 00:59:05 -0800

    test errors

--- exts/events.py
+++ exts/events.py
@@ -146,7 +146,7 @@ class BotEvents:
         import traceback
         if ctx.channel.id == 418452585683484680 and type(error) == discord.ext.commands.errors.CommandNotFound:
             return
-        for page in utils.paginate(traceback.format_tb(error.__traceback__)):
+        for page in utils.paginate(''.join(traceback.format_exception(error))):
             await ctx.send(page)
 
     async def on_guild_join(self, guild):
