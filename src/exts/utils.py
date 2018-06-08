ee258bc62f231a91f5c0737fba65b7807c2a0a81
Dustin Pianalto <dustin@djpianalto.com>
2018-06-08 14:53:06 -0800

    Added space between signature and help

--- src/exts/utils.py
+++ src/exts/utils.py
@@ -749,8 +749,8 @@ class Utils:
 
             if command and not command.hidden:
                 pag.add(f'\uFFF6{command.name}')
-                pag.add(f'Usage: {prefixes.split()[0]}{command.signature}\n\n')
-                pag.add(f'{command.help}')
+                pag.add(f'Usage: {prefixes.split()[0]}{command.signature}\n')
+                pag.add(f'\uFFF0\n{command.help}')
             else:
                 pag.add('\uFFF6There is no command by that name.')
 
