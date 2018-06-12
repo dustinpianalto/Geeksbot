35ab455a7c2ccb7390a1870af53202a34796df6b
Dustin Pianalto <dustin@djpianalto.com>
2018-06-11 17:32:59 -0800

    added help docstring adjusted help line length

--- src/exts/utils.py
+++ src/exts/utils.py
@@ -699,7 +699,8 @@ class Utils:
     @commands.command(name='help', aliases=['h'])
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def custom_help(self, ctx, *, command: str=None):
-        pag = utils.Paginator(self.bot, embed=True, max_line_length=40)
+        """This help message"""
+        pag = utils.Paginator(self.bot, embed=True, max_line_length=48)
         prefixes = await self.bot.get_custom_prefix(self.bot, ctx.message)
         if isinstance(prefixes, list):
             prefixes = ', '.join(prefixes)
