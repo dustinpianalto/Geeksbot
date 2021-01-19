f1980e9bcb4e1c3dca21a4a142ea0ea59124a2c8
Dustin Pianalto <dustin@djpianalto.com>
2021-01-19 00:56:21 -0900

    Remove missing deps

--- internal/exts/init.go
+++ internal/exts/init.go
@@ -2,15 +2,7 @@ package exts
 
 import (
 	"github.com/dustinpianalto/disgoman"
-	"github.com/dustinpianalto/goff/internal/exts/fun"
-	"github.com/dustinpianalto/goff/internal/exts/guild_management"
-	"github.com/dustinpianalto/goff/internal/exts/roles"
-	"github.com/dustinpianalto/goff/internal/exts/tags"
-	"github.com/dustinpianalto/goff/internal/exts/tasks"
-	"github.com/dustinpianalto/goff/internal/exts/user_management"
-	"github.com/dustinpianalto/goff/internal/exts/utils"
-
-	"github.com/dustinpianalto/goff/internal/exts/p_interpreter"
+	"github.com/dustinpianalto/geeksbot/internal/exts/utils"
 )
 
 func AddCommandHandlers(h *disgoman.CommandManager) {
@@ -27,30 +19,4 @@ func AddCommandHandlers(h *disgoman.CommandManager) {
 	_ = h.AddCommand(utils.GitCommand)
 	_ = h.AddCommand(utils.InviteCommand)
 	_ = h.AddCommand(utils.PingCommand)
-	_ = h.AddCommand(tasks.AddReminderCommand)
-	_ = h.AddCommand(tags.AddTagCommand)
-	_ = h.AddCommand(tags.TagCommand)
-	_ = h.AddCommand(roles.MakeRoleSelfAssignableCommand)
-	_ = h.AddCommand(roles.RemoveSelfAssignableCommand)
-	_ = h.AddCommand(roles.SelfAssignRoleCommand)
-	_ = h.AddCommand(roles.UnAssignRoleCommand)
-	_ = h.AddCommand(p_interpreter.PCommand)
-	_ = h.AddCommand(fun.InterleaveCommand)
-	_ = h.AddCommand(fun.DeinterleaveCommand)
-	_ = h.AddCommand(fun.GenerateRPNCommand)
-	_ = h.AddCommand(fun.ParseRPNCommand)
-	_ = h.AddCommand(fun.SolveCommand)
-	_ = h.AddCommand(user_management.KickUserCommand)
-	_ = h.AddCommand(user_management.BanUserCommand)
-	_ = h.AddCommand(user_management.UnbanUserCommand)
-	_ = h.AddCommand(guild_management.SetLoggingChannelCommand)
-	_ = h.AddCommand(guild_management.GetLoggingChannelCommand)
-	_ = h.AddCommand(guild_management.SetWelcomeChannelCommand)
-	_ = h.AddCommand(guild_management.GetWelcomeChannelCommand)
-	_ = h.AddCommand(guild_management.AddGuildCommand)
-	_ = h.AddCommand(guild_management.SetPuzzleChannelCommand)
-	_ = h.AddCommand(guild_management.GetPuzzleChannelCommand)
-	_ = h.AddCommand(guild_management.SetPuzzleRoleCommand)
-	_ = h.AddCommand(guild_management.GetPuzzleRoleCommand)
-
 }
