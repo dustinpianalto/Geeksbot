a6dc6f5e56ed5cad9adab416cbdd88383e00498a
Dustin Pianalto <dustin@djpianalto.com>
2021-01-25 01:52:14 -0900

    fix get user sql query

--- internal/database/channel.go
+++ internal/database/channel.go
@@ -69,8 +69,12 @@ func (s channelService) GuildChannels(g geeksbot.Guild) ([]geeksbot.Channel, err
 
 func (s channelService) GetOrCreateChannel(id string, guild_id string) (geeksbot.Channel, error) {
 	channel, err := s.Channel(id)
+	log.Println(err)
+	log.Println(1)
 	if err.Error() == sql.ErrNoRows.Error() {
 		guild, err := GuildService.GetOrCreateGuild(guild_id)
+		log.Println(err)
+		log.Println(2)
 		if err != nil {
 			return geeksbot.Channel{}, err
 		}
@@ -82,5 +86,7 @@ func (s channelService) GetOrCreateChannel(id string, guild_id string) (geeksbot
 			NewPatron: false,
 		})
 	}
+	log.Println(err)
+	log.Println(3)
 	return channel, err
 }
