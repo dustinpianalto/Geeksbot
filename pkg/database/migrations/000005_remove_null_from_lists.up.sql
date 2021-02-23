04c32955c7032fccf64a97a24047359df297420f
Dustin Pianalto <dustin@djpianalto.com>
2021-02-22 21:29:41 -0900

    Update database schema to not allow null previous_content

--- pkg/database/migrations/000005_remove_null_from_lists.up.sql
+++ pkg/database/migrations/000005_remove_null_from_lists.up.sql
@@ -2,5 +2,5 @@ BEGIN;
     ALTER TABLE messages
         ALTER COLUMN previous_content SET NOT NULL;
     ALTER TABLE messages
-        ALTER COLUMN prevous_content SET DEFAULT array[]::varchar[]
+        ALTER COLUMN prevous_content SET DEFAULT array[]::varchar[];
 COMMIT;
